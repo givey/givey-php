7659eb71ce5a15808e2295ff4ac9b5869ce31ee4
Scott Robertson <scottymeuk@gmail.com>
2015-03-11 23:04:42 +0000

    We don't need extra params anymore

--- src/Entity.php
+++ src/Entity.php
@@ -10,40 +10,11 @@ namespace Givey;
 abstract class Entity
 {
     /**
-     * @var array
-     */
-    private $extraParameters = array();
-
-    /**
      * @param $parameters
      */
     public function __construct($parameters)
     {
         $this->buildParameters($parameters);
-    }
-
-    /**
-     * @param $key
-     *
-     * @return mixed
-     * @throws \Exception
-     */
-    public function _get($key)
-    {
-        if (array_key_exists($key, $this->extraParameters)) {
-            return $this->extraParameters[$key];
-        }
-
-        throw new \Exception('Param does not exist');
-    }
-
-    /**
-     * @param $key
-     * @param $value
-     */
-    public function _set($key, $value)
-    {
-        $this->extraParameters[$key] = $value;
     }
 
     /**
