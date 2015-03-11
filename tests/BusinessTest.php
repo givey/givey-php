541c25d7ec93da2797fff5ac810abf97793e81f9
Scott Robertson <scottymeuk@gmail.com>
2015-03-11 23:03:55 +0000

    Testing Business::employees

--- tests/BusinessTest.php
+++ tests/BusinessTest.php
@@ -14,4 +14,33 @@ class BusinessTest extends AbstractTestCase
         $this->assertInstanceOf('\Givey\Entity\Business', $business);
         $this->assertSame(100, $business->id);
     }
+
+    public function test_employees_returns_array_of_users()
+    {
+        $this->adapter->shouldReceive('get')->with('/businesses/giveyltd')->once()->andReturn(array(
+          'business' => array(
+            'id' => 100
+          )
+        ));
+
+        $business = \Givey\Business::retrieve('giveyltd');
+        $this->assertInstanceOf('\Givey\Entity\Business', $business);
+
+        $this->adapter->shouldReceive('get')
+                      ->with('/users', array('business_id' => $business->id))
+                      ->once()
+                      ->andReturn(array(
+                        'users' => array(
+                          array(
+                            'id' => 100
+                          )
+                        )
+                      ));
+
+        $users = $business->employees();
+        $this->assertInternalType('array', $users);
+        $this->assertCount(1, $users);
+        $this->assertInstanceOf('\Givey\Entity\User', $users[0]);
+        $this->assertSame(100, $users[0]->id);
+    }
 }
