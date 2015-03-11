4a745567409e7338f9601bce2ef0c8bf41545ced
Scott Robertson <scottymeuk@gmail.com>
2015-03-11 22:57:34 +0000

    Test Charity:all

--- tests/CharityTest.php
+++ tests/CharityTest.php
@@ -14,4 +14,21 @@ class CharityTest extends AbstractTestCase
         $this->assertInstanceOf('\Givey\Entity\Charity', $charity);
         $this->assertSame(100, $charity->id);
     }
+
+    public function test_all_returns_array_of_entities()
+    {
+        $this->adapter->shouldReceive('get')->with('/charities', array('business_id' => 100))->once()->andReturn(array(
+          'charities' => array(
+            array(
+              'id' => 100
+            )
+          )
+        ));
+
+        $charities = \Givey\Charity::all(array('business_id' => 100));
+        $this->assertInternalType('array', $charities);
+        $this->assertCount(1, $charities);
+        $this->assertInstanceOf('\Givey\Entity\Charity', $charities[0]);
+        $this->assertSame(100, $charities[0]->id);
+    }
 }
